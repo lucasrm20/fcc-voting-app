bc2daf09b15cc4266ec46d442e6bce8b42a1b0ff
Lucas Rodrigues Moura <lucas.rodrigues20@outlook.com>
2017-06-13 18:01:22 -0300

    extra options script loaded only in the required pages

--- public/js/client.js
+++ public/js/client.js
@@ -1,19 +1,3 @@
 $('document').ready(() => {
-
-    function addMoreOptionInput() {
-        $(this).parent().before(`
-            <div class="has-feedback extra-option">
-                <input type="text" name="poll[options][]" class="form-control" placeholder="Extra Option" required>
-                <span class="glyphicon glyphicon-remove form-control-feedback extra-option-close" aria-hidden="true"></span>
-            </div>
-        `);
-    };
-
-    function removeMoreOptionInput() {
-        $(this).closest('.extra-option').remove();
-    };
-
-    $('#more-options').on('click', addMoreOptionInput);
-    $('.form-group').on('click', '.extra-option-close', removeMoreOptionInput);
-
+    
 });
