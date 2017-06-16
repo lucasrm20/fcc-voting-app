bf7d5570d0523d1eda2c3d36b8506818c99bfdc7
Lucas Rodrigues Moura <lucas.rodrigues20@outlook.com>
2017-06-16 15:48:43 -0300

    404 handler

--- server.js
+++ server.js
@@ -38,6 +38,11 @@ app.use('/polls', require('./routes/polls'));
 app.use('/auth', require('./routes/auth'));
 app.use('/users', require('./routes/users'));
 
+// ERROR HANDLERS
+app.use((req, res, next) => {
+    res.status(404).render('errors/404');
+});
+
 const listener = app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${listener.address().port}`);
 });
